77f7b8ca751d8442eb2e5645718a3e7ceb2e9689
Prabeen banchhor <prabeen@greedygame.com>
2023-05-23 18:07:55 +0530

    unemployed iframe aded

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 console.log("test.js");
 
-window.addEventListener(
-  "message",
-  (e) => {
-    console.log(e);
-    console.log(e.data);
-    console.log(e.data && e.data.AMP_SCRIPT); // Check if e.data is defined before accessing the property
-  },
-  false
-);
+// window.addEventListener(
+//   "message",
+//   (e) => {
+//     // console.log(e);
+//     // console.log(e.data);
+//     // console.log(e.data && e.data.AMP_SCRIPT); // Check if e.data is defined before accessing the property
+//   },
+//   false
+// );
